7b462d6abd8186172053ae250371c6167838c478
Huy Tuyen <fsoft.datnn@gmail.com>
2022-12-19 13:10:07 +0700

    update QR

--- pages/[id]/qr.jsx
+++ pages/[id]/qr.jsx
@@ -1,9 +1,7 @@
-import {
-  Box, Container, Heading, GridItem, Button
-} from '@chakra-ui/react';
+import { Box, Container, Heading, GridItem, Button } from '@chakra-ui/react';
 import _ from 'lodash';
 
-import Layout from '@/components/Layout'
+import Layout from '@/components/Layout';
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import QRCode from 'react-qr-code';
@@ -16,21 +14,21 @@ const QR = (props) => {
 
   useEffect(() => {
     setQrCode(_.replace(window.location.href, '/qr', ''));
-  })
+  });
 
   const onImageCownload = () => {
-    const svg = document.getElementById("QRCode");
+    const svg = document.getElementById('QRCode');
     const svgData = new XMLSerializer().serializeToString(svg);
-    const canvas = document.createElement("canvas");
-    const ctx = canvas.getContext("2d");
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
     const img = new Image();
     img.onload = () => {
       canvas.width = img.width;
       canvas.height = img.height;
       ctx.drawImage(img, 0, 0);
-      const pngFile = canvas.toDataURL("image/png");
-      const downloadLink = document.createElement("a");
-      downloadLink.download = "QRCode";
+      const pngFile = canvas.toDataURL('image/png');
+      const downloadLink = document.createElement('a');
+      downloadLink.download = 'QRCode';
       downloadLink.href = `${pngFile}`;
       downloadLink.click();
     };
@@ -45,12 +43,9 @@ const QR = (props) => {
         backgroundSize={'cover'}
         backgroundPosition={'center'}
         height={'100%'}
+        p={[2, 4, 6, 8]}
       >
-        <Container
-          maxW={'4xl'}
-          pt={{ base: 5 }}
-          alignItems={'center'}
-        >
+        <Container maxW={'lg'} pt={{ base: 5 }} alignItems={'center'}>
           <GridItem colSpan={{ base: 3 }}>
             <Box
               bg={'gray.50'}
@@ -58,25 +53,36 @@ const QR = (props) => {
               roundedTop={'5px'}
             >
               <Box textAlign={'center'}>
-              <Heading fontSize={{ base: '30px', sm: '20px', md: '25px' }} color='blue.500'>
-                {`Quét má QR lấy link đăng ký quy y`}
-              </Heading>
+                <Heading
+                  fontSize={{ base: '30px', sm: '20px', md: '25px' }}
+                  color="blue.500"
+                >
+                  {`Quét má QR lấy link đăng ký quy y`}
+                </Heading>
               </Box>
-              <Box display={'none'}><QRCode id="QRCode" title="Custom Title" value={urlQrCode} /></Box>
-              <Box p={3} textAlign='center'>
+              <Box display={'none'}>
+                <QRCode id="QRCode" title="Custom Title" value={urlQrCode} />
+              </Box>
+              <Box p={3} textAlign="center">
                 <QRCode
-                  id='QRCode'
+                  id="QRCode"
                   // size={256}
                   style={{ height: 'auto', maxWidth: '100%', width: '100%' }}
                   value={urlQrCode}
-                // viewBox={`0 0 256 256`}
+                  // viewBox={`0 0 256 256`}
                 />
               </Box>
               <Box textAlign={'center'}>
-            <Button mt={5} width='200px' colorScheme='blue' onClick={() => onImageCownload()} mr={3}>
-                Lưu về máy
-              </Button>
-            </Box>
+                <Button
+                  mt={5}
+                  width="200px"
+                  colorScheme="blue"
+                  onClick={() => onImageCownload()}
+                  mr={3}
+                >
+                  Lưu về máy
+                </Button>
+              </Box>
             </Box>
           </GridItem>
         </Container>
